3b9d49e6bfd2d9ceb923ceabcb3c35f67adece20
Betty <betty4web@gmail.com>
2026-01-19 16:56:45 +0100

    chore: add Tailwind CSS integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import tailwind from '@tailwindcss/vite'; 
 
-// https://astro.build/config
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
-  adapter: cloudflare(),
   output: 'static',
+  adapter: isProd ? cloudflare({ mode: 'directory' }) : undefined,
+  vite: {
+    plugins: [tailwind()], 
+  },
 });
