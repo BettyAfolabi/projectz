1e86679f77003255b99110caeddd61b32a26df72
Betty <betty4web@gmail.com>
2026-04-18 18:06:15 +0100

    build: removed astro adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'static',
-  adapter: cloudflare({ mode: 'directory' }),
   integrations: [tailwind(), mdx(), react()],
 });
